ddce31e80d8f79b1f4ccc9d443fca4463bd3cc53
tsaezi <t.saezi@udd.cl>
2025-11-21 11:36:54 -0300

    Add files via upload

---
+++ Ayudantías/EjercicioEnClase.py
@@ -0,0 +1,9 @@
+#Consideren las siguientes cadenas de texto
+
+cadena1 = "El avance de la inteligencia artificial ha transformado numerosos sectores en las últimas décadas. Las máquinas ahora pueden realizar tareas que antes eran exclusivas de los humanos, como el reconocimiento de voz y la traducción automática. A medida que estas tecnologías evolucionan, también surgen desafíos éticos relacionados con su implementación. La educación, la medicina y la industria manufacturera son algunos de los campos que más se han beneficiado de esta revolución tecnológica."
+
+
+cadena2 = "La educación, la medicina y la industria manufacturera son algunos de los campos que más se han beneficiado de esta revolución tecnológica. Además, el reconocimiento de voz y la traducción automática son ejemplos de tareas que ahora pueden ser realizadas por máquinas. A medida que estas tecnologías evolucionan, también surgen desafíos éticos relacionados con su implementación. El impacto de la inteligencia artificial se extiende a muchos aspectos de nuestra vida cotidiana."
+
+#utilizando ciclos for, genere un script que permita identificar coincidencias entre ambas
+#almacene estas coincidencias en una lista siempre y cuando su largo sea mayor a 7.
